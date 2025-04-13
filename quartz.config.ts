0a0c373b1c4da17bd2acf063cbc771c629cc82b2
Daph <154984490+calcknight@users.noreply.github.com>
2025-04-13 10:55:51 +0300

    Update quartz.config.ts
    changed fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Silkscreen",
+        body: "Work Sans",
         code: "IBM Plex Mono",
       },
       colors: {
